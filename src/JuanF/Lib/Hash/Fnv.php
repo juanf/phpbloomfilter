ed3ad2343fe31d0687360102f182966cfdf8445a
Juan <juan.ferrarib@gmail.com>
2015-12-16 11:17:28 -0300

    Changed hash algo to fnv132 instead of fnv1a32 to avoid this: https://bugs.php.net/bug.php?id=66698 on older php versions

--- src/JuanF/Lib/Hash/Fnv.php
+++ src/JuanF/Lib/Hash/Fnv.php
@@ -28,6 +28,6 @@ class Fnv extends Hash
      */
     public static function hash($txt)
     {
-        return sprintf('%u', hexdec(hash('fnv1a32', $txt)));
+        return sprintf('%u', hexdec(hash('fnv132', $txt)));
     }
 }
